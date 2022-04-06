6dbfb004be7cba72a7f24f053fd3823f25d90bba
Elisee-dev <esabidani@gmail.com>
2022-04-06 22:02:30 +0200

    Debut __section__testimonials

--- src/components/testimonies/Testimonies.jsx
+++ src/components/testimonies/Testimonies.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import AVTR1 from "../../assets/avatar1.jpg"
-// import AVTR2 from "../../assets/avatar.2jpg"
-// import AVTR3 from "../../assets/avatar.j3pg"
-// import AVTR4 from "../../assets/avatar.jp4g"
+
+import "./testimonies.css"
 
 const Testimonies = () => {
     return (
@@ -13,38 +12,7 @@ const Testimonies = () => {
             <div className="container testimonials__container">
                 <article className="testimonial">
                     <div className="client__avatar">
-                        <img src={AVTR1} alt="Avatar One" />
-                        <h5 className="client__name">Ernest Achiev</h5>
-                        <small className="client_review">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga illo porro corporis minus voluptas veniam deserunt laudantium! Distinctio dignissimos vitae commodi esse blanditiis aperiam deserunt maxime dolores. Officia, esse aliquid.
-                        </small>
-                    </div>
-                </article>
-                <article className="testimonial">
-                    <div className="client__avatar">
-                        <img src={AVTR1} alt="Avatar One" />
-                        <h5 className="client__name">Ernest Achiev</h5>
-                        <small className="client_review">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga illo porro corporis minus voluptas veniam deserunt laudantium! Distinctio dignissimos vitae commodi esse blanditiis aperiam deserunt maxime dolores. Officia, esse aliquid.
-                        </small>
-                    </div>
-                </article>
-                <article className="testimonial">
-                    <div className="client__avatar">
-                        <img src={AVTR1} alt="Avatar One" />
-                        <h5 className="client__name">Ernest Achiev</h5>
-                        <small className="client_review">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga illo porro corporis minus voluptas veniam deserunt laudantium! Distinctio dignissimos vitae commodi esse blanditiis aperiam deserunt maxime dolores. Officia, esse aliquid.
-                        </small>
-                    </div>
-                </article>
-                <article className="testimonial">
-                    <div className="client__avatar">
-                        <img src={AVTR1} alt="Avatar One" />
-                        <h5 className="client__name">Ernest Achiev</h5>
-                        <small className="client_review">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga illo porro corporis minus voluptas veniam deserunt laudantium! Distinctio dignissimos vitae commodi esse blanditiis aperiam deserunt maxime dolores. Officia, esse aliquid.
-                        </small>
+                        
                     </div>
                 </article>
             </div>
